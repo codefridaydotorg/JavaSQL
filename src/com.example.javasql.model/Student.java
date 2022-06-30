96416d46142dee816d7b24290a94fc03a7e4ba3e
FalseG0d <garg.apoorv1098@gmail.com>
2022-06-30 22:02:06 +0530

    Java SQL

---
+++ src/com.example.javasql.model/Student.java
@@ -0,0 +1,41 @@
+package com.example.javasql.model;
+
+public class Student {
+    private int id;
+    private String fname;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public int getFee() {
+        return fee;
+    }
+
+    public void setFee(int fee) {
+        this.fee = fee;
+    }
+
+    private String course;
+    private int fee;
+}
